0ad4cf423ddcbc35d6b6776b74e61e2260d1affe
jcp05131 <jcp05131@castleton.edu>
2022-10-19 11:38:26 -0400

    Lab 7 Work

--- dictionary-binary-search.js
+++ dictionary-binary-search.js
@@ -1,7 +1,18 @@
 import words from "./words.js";
 
 function binarySearch(needle, haystack){
-    //❓ Question 7, write it!
+    let low = 0;
+    let high = haystack.length-1;
+    while true{
+        let mid = ((high + low)/2);
+        Math.floor(mid);
+        if(haystack[mid]==needle)
+        return true;
+        if(haystack[mid < needle])
+        start mid;
+        if(haystack[mid > needle])
+        end mid;
+        if start = end;
     return false;
 }
 
--- dictionary-index-search.js
+++ dictionary-index-search.js
@@ -1,28 +1,37 @@
 import words from "./words.js";
 
-function getPositionInAlphabet(word){
+function getPositionInAlphabet(word) {
     //This is something you have not seen before. Don't worry.
     return word.charCodeAt(0) - "a".charCodeAt(0);
 }
 
-function createIndexForDictionary(d){
+function createIndexForDictionary(d) {
     let index = [0];
     let position = 0;
-    for( let i = 1; i <= 26; i++){
-        while ( position < d.length && getPositionInAlphabet(d[position]) < i )
+    for (let i = 1; i <= 26; i++) {
+        while (position < d.length && getPositionInAlphabet(d[position]) < i)
             position++;
         index[i] = position;
     }
     return index;
 }
 
-function indexSearch(needle, haystack, index){
-    //❓ Question 6 TODO: Finish this code
+function indexSearch(needle, haystack, index) {
+    let wordposition = getPositionInAlphabet(needle);
+
+    let startPosition = index[wordposition];
+    let endPosition = index[wordposition + 1];
+
+    for (let i = startPosition; i < endPosition; i++) {
+        if (haystack[i] == needle) {
+            return true;
+        }
+    }
     return false;
 }
 
 let index = createIndexForDictionary(words);
 
-export default function(word){
+export default function (word) {
     return indexSearch(word, words, index);
 }
